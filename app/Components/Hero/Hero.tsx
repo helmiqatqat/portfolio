a05b3ab7e99050c673c4a45007060ce9888129f3
Helmi-Qatqat <helmiqatqat@gmail.com>
2023-11-07 00:16:15 +0000

    test

--- app/Components/Hero/Hero.tsx
+++ app/Components/Hero/Hero.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link';
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import laptop from '@/public/Assets/SVG/laptop.svg'
 import Wave from '@/public/Background/Wave';
 
@@ -14,8 +14,7 @@ export default function Hero() {
       <div className='grid-hero-child w-full h-full items-center'>
         <div className='flex justify-center md:justify-start col-span-12 md:col-span-8'>
           <div className='flex flex-col items-center gap-6 lg:gap-8'>
-            {/*  */}
-            <motion.h1
+            {/* <motion.h1
               initial={{ opacity: 0, x: -100 }}
               animate={{ opacity: 1, x: 0 }}
               transition={{ duration: 1 }}
@@ -36,7 +35,7 @@ export default function Hero() {
                   {letter === " " ? "\u00A0" : letter}
                 </motion.span>
               ))}
-            </motion.p>
+            </motion.p> */}
             <Link
               href="/Assets/PDF/HelmiQatqatCV.pdf"
               target="_blank"
